e6370a0cf8e3f5d1ace69fd6e642cb29956a7cb5
bozmix <mijailovic.bozidar@gmail.com>
2022-03-04 23:02:44 +0100

    arrays and loops exercizes

---
+++ PP/arrays/arrays.js
@@ -0,0 +1,37 @@
+/*
+Write an array of months in a year. Using console.log display June, October and January from the array.
+*/
+
+var months = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'];
+
+console.log(months[5], months[9], months[0]);
+
+/*
+Write an arrays of days in a week. Using console.log display Sunday from the array.
+
+ Print all negative elements from the array [2, -4, 5, -2, -11].  
+*/
+
+var arr = [2, -4, 5, -2, -11];
+
+console.log(arr[1], arr[3], arr[4]);
+
+/*
+Print all elements with indices divisible by 3 from the array [5, 15, -5, 20, 12, 18, 72, 14, 9]. 
+
+What is the index of number 24 in the following array?
+[[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+*/
+
+var arr = [[1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 26, 7]];
+
+console.log(arr[0][3]);
+
+/*
+Using console.log:
+Display the 3rd element of the array,
+Display the 2nd element of the 3rd element.
+*/
+
+console.log(arr[2]);
+console.log(arr[2][1]);
---
+++ PP/loops/loops.js
@@ -0,0 +1,89 @@
+/*
+Write a for loop that will iterate from 0 to 15. For each iteration, it will check if the current number is odd or even, and display a message to the screen.
+*/
+
+for (var i = 0; i <= 15; i++){
+    if (i % 2 === 0){
+        console.log('the number ' + i + ' is even');
+    } else {
+        console.log('the number ' + i + ' is odd');
+    }
+}
+
+/*
+Write a program to sum the multiples of 3 and 5 under 1000.
+*/
+
+var sum = 0;
+
+for (var i = 0; i < 1000; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log(i + ' this is i');
+        sum += i;
+        console.log(sum + ' this is sum')
+    }
+}
+
+console.log(sum);
+
+/*
+Write a program to compute the sum and product of an array of integers.
+*/
+
+var arr = [1, 2, 3, 4, 5];
+var sum = 0, prod = 1;
+
+for (var i = 0; i < arr.length; i++){
+    sum += arr[i];
+    prod *= arr[i];
+}
+
+console.log(sum, prod);
+/*
+Write a program which prints the elements of the following array as a single string.
+
+var x = ['1', 'A', 'B', "c", "r", true, NaN, undefined];
+
+Write a program that prints the elements of the following array.
+
+var a = [
+[1, 2, 1, 24], 
+[8, 11, 9, 4], 
+[7, 0, 7, 27]
+];
+
+Write a program that outputs the sum of squares of the first 20 numbers. 
+
+Write a program that computes average marks of the following students. Then use this average to determine the corresponding grade. 
+	
+David
+80
+Marko
+77
+Dany
+88
+John
+95
+Thomas
+68
+
+
+The grades are computed as follows :
+
+< 60%
+F
+< 70%
+D
+< 80%
+C
+< 90%
+B
+< 100%
+A
+
+
+
+Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. When you have that working, modify your program to print "FizzBuzz", for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
+
+Note: This is actually an interview question that has been claimed to weed out a significant percentage of programmer candidates. So if you’ve solved it, you’re now allowed to feel good about yourself.
+*/
