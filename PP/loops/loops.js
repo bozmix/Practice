8baefb569ac47ab1e40c9411e4d99474c6178d36
bozmix <mijailovic.bozidar@gmail.com>
2022-03-13 15:50:37 +0100

    loops exercises

--- PP/loops/loops.js
+++ PP/loops/loops.js
@@ -43,7 +43,18 @@ console.log(sum, prod);
 Write a program which prints the elements of the following array as a single string.
 
 var x = ['1', 'A', 'B', "c", "r", true, NaN, undefined];
+*/
+var x = ['1', 'A', 'B', "c", "r", true, NaN, undefined];
 
+var output = '';
+
+for (var i = 0; i < x.length; i++){
+    output += x[i];
+}
+console.log(output);
+
+
+/*
 Write a program that prints the elements of the following array.
 
 var a = [
@@ -51,9 +62,37 @@ var a = [
 [8, 11, 9, 4], 
 [7, 0, 7, 27]
 ];
+*/
+var a = [
+    [1, 2, 1, 24], 
+    [8, 11, 9, 4], 
+    [7, 0, 7, 27]
+];
 
+for (var i = 0; i < a.length; i++){
+    console.log(a[i]);
+}
+
+for (var i = 0; i < a.length; i++){
+    for (var j = 0; j < a[i].length; j++){
+        console.log(a[i][j])
+    }
+}
+
+/*
 Write a program that outputs the sum of squares of the first 20 numbers. 
+*/
+var sumOfSquares = 0;
 
+for (var i = 1; i <= 20; i++){
+    var square = i * i;
+    console.log(sumOfSquares);
+    sumOfSquares += square;
+    console.log(sumOfSquares);
+}
+
+console.log(sumOfSquares);
+/*
 Write a program that computes average marks of the following students. Then use this average to determine the corresponding grade. 
 	
 David
@@ -80,10 +119,24 @@ C
 B
 < 100%
 A
+*/
 
-
-
+/*
 Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead. When you have that working, modify your program to print "FizzBuzz", for numbers that are divisible by both 3 and 5 (and still print "Fizz" or "Buzz" for numbers divisible by only one of those).
 
 Note: This is actually an interview question that has been claimed to weed out a significant percentage of programmer candidates. So if you’ve solved it, you’re now allowed to feel good about yourself.
 */
+for (var i = 1; i <= 100; i++){
+    if(i % 3 === 0){
+        if(i % 5 === 0){
+            console.log('FizzBuzz');
+        } else {
+            console.log('Fizz');
+        } 
+    } else if(i % 5 === 0){
+        console.log('Buzz');
+    } else {
+        console.log(i);
+    }
+    
+}
